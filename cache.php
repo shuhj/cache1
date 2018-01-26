b6c65995897e101dbfd698c5274268cf5a184351
shj <shj@qq.com>
2018-01-26 14:47:22 +0800

    修改注释

--- cache.php
+++ cache.php
@@ -6,13 +6,14 @@
 
 
 //连接数据库
-function data(){
+function data()
+{
     $link = @mysql_connect('localhost', 'root', 'root') or die('数据库连接失败');
     mysql_select_db('music', $link);
     mysql_set_charset('utf8');
     $sql = 'select * from music';
     $data = mysql_query($sql);
-    while ($x = mysql_fetch_array($data,MYSQL_ASSOC)) {
+    while ($x = mysql_fetch_array($data, MYSQL_ASSOC)) {
         $music[] = $x;
     }
     return $music;
@@ -21,7 +22,7 @@ function data(){
 
 $file = './test.html';
 
-if (is_file($file)&&time()-filemtime($file)<10) {
+if (is_file($file) && time() - filemtime($file) < 100) {
     require_once $file;
 } else {
     //调用上面的函数
@@ -33,7 +34,7 @@ if (is_file($file)&&time()-filemtime($file)<10) {
     $res = fopen($file, 'w+');
     require_once 'test.php';
 
-    //将缓冲区内容写入文件
+    //将缓冲区内容写入到缓存文件（下次从缓存文件中取数据）
     fwrite($res, ob_get_contents());
 
     fclose($res);
